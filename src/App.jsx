ae638985814d777fea56e0f47ab612163899f5e0
RPJunco <rodrigojunco023@gmail.com>
2024-07-17 18:34:17 -0300

    Se creo un proyecto utilizando React. El proyecto se creo utilizando las herramientas principales (react, node.js ,etc), En el mismo se creo la estructura principal del trabajo y luego se modificaron diversos archivos, como el contenido del archivo 'App.jsx' entre otros

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './Header/Header.jsx'
+import './Main/Main.jsx'
+import './Footer/Footer.jsx'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
