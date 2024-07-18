51441de298f7801c06c011c92af2f72dac324b07
RPJunco <rodrigojunco023@gmail.com>
2024-07-17 21:44:18 -0300

    Se renderizo el componente 'Main', importado directamente de la ruta './src/Component/Main'.

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import Header from './Component/Header/Header.jsx'
 import Footer from './Component/Footer/Footer.jsx'
+import Main from './Component/Main/Main.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Main />
     <Footer />
     <App />
   </React.StrictMode>,
