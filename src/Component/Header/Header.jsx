a5e14a481120605661ed23e0310f5843efb5157a
RPJunco <rodrigojunco023@gmail.com>
2024-07-18 18:15:44 -0300

    Se cambio la estructura del componente 'Header.jsx'. Se importo bootstrap, luego se cambio la estructura utilizando elementos de bootstrap

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,21 +1,40 @@
 import './Header.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
     return (    
-        <header class="header">
+        <header className="header" id='header'>
 
-            <img src="./img/logo.PNG" alt="logo-portofolio" />
-            
-            <nav class="navegacion-secciones">
-                
-                <a href="#inicio">Inicio</a>
-                <a href="#acercaDe">Acerca de</a>
-                <a href="#servicios">Servicio</a>
-                <a href="#skills">Habilidades</a>
-                <a href="#portfolio">Portfolio</a>
-                <a href="#contacto">Contacto</a>
-                <i class="bi bi-list menu-icono"></i>
-                
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#"><img src="./img/logo.PNG" alt="logo-portofolio" /></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <form className="d-flex" role="links">
+                        <ul className='nav'>
+                            <li className='nav-item'>
+                                <a href="#inicio" className='nav-link'>Inicio</a>
+                            </li>
+                            <li className='nav-item'>
+                                <a href="#acercaDe" className='nav-link'>Acerca de</a>
+                            </li>
+                            <li className='nav-item'>
+                                <a href="#servicios" className='nav-link'>Servicio</a>
+                            </li>
+                            <li className='nav-item'>
+                                <a href="#skills" className='nav-link'>Habilidades</a>
+                            </li>
+                            <li className='nav-item'>
+                                <a href="#portfolio" className='nav-link'>Portfolio</a>
+                            </li>
+                            <li className='nav-item'>
+                                <a href="#contacto" className='nav-link'>Contacto</a>
+                                <i className="bi bi-list menu-icono"></i>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
             </nav>
 
     </header>
