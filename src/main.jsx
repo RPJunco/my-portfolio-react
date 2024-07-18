5093250215a3d1127aa33b94c91ba807f161e1cc
RPJunco <rodrigojunco023@gmail.com>
2024-07-17 21:34:31 -0300

    Se renderizo el componente Footer importado desde la ruta './src/Component/Footer'.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Header from './Component/Header/Header.jsx'
+import Footer from './Component/Footer/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Footer />
     <App />
   </React.StrictMode>,
 )
