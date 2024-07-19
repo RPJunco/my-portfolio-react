c13b9ac6bab857ed627741d22d3b13391b73a32b
RPJunco <rodrigojunco023@gmail.com>
2024-07-19 18:15:26 -0300

    Se modifico la estructura del componente con bootstrap

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -5,36 +5,37 @@ import Btn from '../Btn/Btn.jsx'
 const Footer = () => {
     return (
         <footer className='footer' id='contacto'>
+            
+            <div className='container-fluid justify-content-center align-items-center w-75'>
+                <h2>Interesante verdad? <span>Contactame!</span></h2>
 
-            <h2>Interesante verdad? <span>Contactame!</span></h2>
+                <div className='container-fluid justify-content-center align-items-center flex-wrap w-75 p-3'>
 
-            <div className='container-fluid'>
+                    <div className="form-floating">
+                        <input type="text" className="form-control" id="floatingName" placeholder="Pedro" />
+                        <label htmlFor="floatingName">Nombre</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="text" className="form-control" id="floatingLastName" placeholder="Pascal" />
+                        <label htmlFor="floatingLastName">Apellido</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="tel" className="form-control" id="floatingPhoneNumber" placeholder="351*******" />
+                        <label htmlFor="floatingPhoneNumber">Telefono de contacto</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="email" className="form-control" id="floatingEmail" placeholder="Email" />
+                        <label htmlFor="floatingEmail">Direccion de email</label>
+                    </div>
 
-                <div className="form-floating">
-                    <input type="text" className="form-control" id="floatingName" placeholder="Pedro" />
-                    <label htmlFor="floatingName">Nombre</label>
-                </div>
-                <div className="form-floating">
-                    <input type="text" className="form-control" id="floatingLastName" placeholder="Pascal" />
-                    <label htmlFor="floatingLastName">Apellido</label>
-                </div>
-                <div className="form-floating">
-                    <input type="tel" className="form-control" id="floatingPhoneNumber" placeholder="351*******" />
-                    <label htmlFor="floatingPhoneNumber">Telefono de contacto</label>
-                </div>
-                <div className="form-floating">
-                    <input type="email" className="form-control" id="floatingEmail" placeholder="Email" />
-                    <label htmlFor="floatingEmail">Direccion de email</label>
                 </div>
 
+                <div className="form-floating form-container container-fluid justify-content-center w-75">
+                    <textarea className="form-control" placeholder="Deja un comentario!!" id="floatingTextarea"></textarea>
+                    <label htmlFor="floatingTextarea" className='label-textarea'>Comments</label>
+                </div>
+                <Btn contenido='Enviar Mensaje'/>
             </div>
-
-            <div className="form-floating form-container">
-                <textarea className="form-control" placeholder="Deja un comentario!!" id="floatingTextarea"></textarea>
-                <label htmlFor="floatingTextarea">Comments</label>
-            </div>
-            <Btn contenido='Enviar Mensaje'/>
-
         </footer>
     )
 };
