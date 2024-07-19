1e50d3d0978776acac56cafef0694cef49185c5e
RPJunco <rodrigojunco023@gmail.com>
2024-07-19 19:24:18 -0300

    Se agrego un prop para manejar la propiedad 'width' del componente 'Btn'

--- src/Component/Btn/Btn.jsx
+++ src/Component/Btn/Btn.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import './Btn.css'
 
-const Btn = ({ contenido }) => {
+const Btn = ({ contenido, width }) => {
     return (
-        <div className="d-grid">
+        <div className="d-grid" style={{ width:width }}>
             <button className="boton" type="button">{contenido}</button>
         </div>
     )
