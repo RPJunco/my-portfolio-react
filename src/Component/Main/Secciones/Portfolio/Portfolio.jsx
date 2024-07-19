d88735b5baf1b785325d8984e5bc1d3e5f4240d6
RPJunco <rodrigojunco023@gmail.com>
2024-07-19 19:52:16 -0300

    Se modifico la estructura del componente 'Portfolio' utilizando bootstrap

--- src/Component/Main/Secciones/Portfolio/Portfolio.jsx
+++ src/Component/Main/Secciones/Portfolio/Portfolio.jsx
@@ -2,36 +2,36 @@ import './Portfolio.css'
 
 const Portfolio = () => {
     return (
-        <section id="portfolio">
+        <section id="portfolio" className='portfolio container-fluid'>
 
-            <section className="portfolio-contenedor">
+            <section className="d-flex flex-column">
 
-                <h2>Mi <span>Portfolio</span></h2>
+                <h2 className='m-5'>Mi <span>Portfolio</span></h2>
 
-                <section className="portfolio-proyectos">
+                <section className="d-flex flex-wrap mb-4">
 
-                    <div>
-                        <a href="https://rpjunco.github.io/Travel-Agency-Login-page/" target="_blank"><img src="./img/portfolio/travel-agency-login-page.PNG" class="img-proyecto" alt="Travel-Agency-Login-page" /></a>
+                    <div className='flex-grow-1'>
+                        <a href="https://rpjunco.github.io/Travel-Agency-Login-page/" target="_blank"><img src="./img/portfolio/travel-agency-login-page.PNG" className="img-proyecto" alt="Travel-Agency-Login-page" /></a>
                     </div>
-                    <div>
-                        <a href="https://rpjunco.github.io/What-we-offer-section-design/" target="_blank"><img src="./img/portfolio/what-do-we-offer.PNG" class="img-proyecto" alt="What-dowe-offer" /></a>
+                    <div className='flex-grow-1'>
+                        <a href="https://rpjunco.github.io/What-we-offer-section-design/" target="_blank"><img src="./img/portfolio/what-do-we-offer.PNG" className="img-proyecto" alt="What-dowe-offer" /></a>
                     </div>
-                    <div>
-                        <a href="https://github.com/RPJunco/soccer-card-menu/tree/main" target="_blank"><img src="./img/portfolio/soccer-cards-menu.PNG" class="img-proyecto" alt="soccer-cards-menu" /></a>
+                    <div className='flex-grow-1'>
+                        <a href="https://github.com/RPJunco/soccer-card-menu/tree/main" target="_blank"><img src="./img/portfolio/soccer-cards-menu.PNG" className="img-proyecto" alt="soccer-cards-menu" /></a>
                     </div>
 
                 </section>
 
-                <section className="portfolio-proyectos">
+                <section className="d-flex flex-wrap mb-5">
 
-                    <div>
-                        <a href="https://github.com/RPJunco/records-management/tree/main" target="_blank"><img src="./img/portfolio/records-management.PNG" class="img-proyecto" alt="records-management" /></a>
+                    <div className='flex-grow-1'>
+                        <a href="https://github.com/RPJunco/records-management/tree/main" target="_blank"><img src="./img/portfolio/records-management.PNG" className="img-proyecto" alt="records-management" /></a>
                     </div>
-                    <div>
-                        <a href="https://github.com/RPJunco/character-and-list-processing/tree/main" target="_blank"><img src="./img/portfolio/character-treatment.PNG" class="img-proyecto" alt="character-and-list-processing" /></a>
+                    <div className='flex-grow-1'>
+                        <a href="https://github.com/RPJunco/character-and-list-processing/tree/main" target="_blank"><img src="./img/portfolio/character-treatment.PNG" className="img-proyecto" alt="character-and-list-processing" /></a>
                     </div>
-                    <div>
-                        <a href="https://github.com/RPJunco/article-management" target="_blank"><img src="./img/portfolio/strings-treatment.PNG" class="img-proyecto" alt="strings-treatment" /></a>
+                    <div className='flex-grow-1'>
+                        <a href="https://github.com/RPJunco/article-management" target="_blank"><img src="./img/portfolio/strings-treatment.PNG" className="img-proyecto" alt="strings-treatment" /></a>
                     </div>
 
                 </section>
