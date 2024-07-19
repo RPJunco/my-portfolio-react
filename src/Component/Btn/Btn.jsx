c69df2c0ce3e9926039f485cf551b34abd10d574
RPJunco <rodrigojunco023@gmail.com>
2024-07-19 00:33:26 -0300

    Se creo un componente llamado 'Btn.jsx'. El mismo se almacenara en una carpeta llamada 'Btn' junto a un archivo que le agregara estilos al boton

---
+++ src/Component/Btn/Btn.jsx
@@ -0,0 +1,11 @@
+import './Btn.css'
+
+const Btn = () => {
+    return (
+        <div className="d-grid">
+            <button className="btn" type="button"></button>
+        </div>
+    )
+};
+
+export default Btn;
