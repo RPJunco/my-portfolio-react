a64d7bf3bef45257390ad3d9c4a5ad91a5ccc0b4
RPJunco <rodrigojunco023@gmail.com>
2024-07-19 19:08:54 -0300

    Se importaron los componentes 'Main', 'Footer' y 'Header' al componente 'App.jsx' y son utilizados dentro de su return

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './Component/Header/Header.jsx'
 import './Component/Main/Main.jsx'
 import './Component/Footer/Footer.jsx'
 import './App.css'
+import Header from './Component/Header/Header.jsx'
+import Footer from './Component/Footer/Footer.jsx'
+import Main from './Component/Main/Main.jsx'
 
 function App() {
   
 
   return (
     <>
-      
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
