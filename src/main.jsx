980d600cf6981cd49a6555cae60448b7b9a530f8
RPJunco <rodrigojunco023@gmail.com>
2024-07-19 19:09:58 -0300

    Se eliminaron los componentes 'Main', 'Footer' y 'Header' del archivo, fueron trasladados al componentes 'App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Header from './Component/Header/Header.jsx'
-import Footer from './Component/Footer/Footer.jsx'
-import Main from './Component/Main/Main.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
-    <Main />
-    <Footer />
     <App />
   </React.StrictMode>,
 )
