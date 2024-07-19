9fc29bac14e6802f6556d7622eaa6f883858a31c
RPJunco <rodrigojunco023@gmail.com>
2024-07-19 00:57:19 -0300

    Se modifico la variable cont

--- src/Component/Btn/Btn.jsx
+++ src/Component/Btn/Btn.jsx
@@ -1,7 +1,7 @@
 import './Btn.css'
 
 const Btn = (props) => {
-    let cont = props;
+    let cont = props.contenido;
     return (
         <div className="d-grid">
             <button className="btn" type="button">{cont}</button>
