7796a49ba4868426ed31f1dc46d4b53dead4f697
RPJunco <rodrigojunco023@gmail.com>
2024-07-19 14:48:51 -0300

    Se modifico la estructura del componente utilizando bootstrap para algunas etiquetas.

--- src/Component/Main/Secciones/Work/Work.jsx
+++ src/Component/Main/Secciones/Work/Work.jsx
@@ -1,30 +1,30 @@
 import './Work.css'
+import Btn from '../../../Btn/Btn.jsx';
 
 const Work = () => {
     return (
-        <section id="servicios">
+        <section id="servicios" className='servicios'>
 
             <h2>Mis <span>Servicios</span></h2>
 
-            <section className="servicio-contenedor">
+            <section className="container-fluid">
 
-                <div className="btn1">
+                <div className="card mb-4">
                     <h3>Desarrollo de aplicaciones FrontEnd</h3>
-                    <a href="#"  className="boton">Leer Mas</a>
+                    <Btn contenido='Leer Mas'/>
                 </div>
-                <div className="btn2">
+                <div className="card mb-4">
                     <h3>Desarrollo de aplicaciones BackEnd</h3>
-                    <a href="#"  className="boton">Leer Mas</a>
+                    <Btn contenido='Leer Mas'/>
                 </div>
-                <div className="btn3">
+                <div className="card mb-4">
                     <h3>Desarrollo de aplicaciones FullStack</h3>
-                    <a href="#"  className="boton">Leer Mas</a>
+                    <Btn contenido='Leer Mas'/>
                 </div>
+
+            </section>
 
         </section>
-
-
-    </section>
     )
 };
 
