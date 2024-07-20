08bba7cb42e909361af2d1913ffabb2b0ab5ad3d
RPJunco <rodrigojunco023@gmail.com>
2024-07-20 14:00:26 -0300

    Se modifico la estructura del componente 'About' agregandole bootstrap

--- src/Component/Main/Secciones/About/About.jsx
+++ src/Component/Main/Secciones/About/About.jsx
@@ -3,27 +3,27 @@ import Btn from '../../../Btn/Btn.jsx'
 
 const About = () => {
     return (
-        <section id="acercaDe" className='container-fluid'>
-        
-            <section className="foto-contenedor">
-                <img src="./img/rodrigo-junco.jpg" alt="foto-perfil" />
-            </section>
+        <section id="acercaDe" className="container-fluid">
+            <div className="row align-items-center">
+                <section className="foto-contenedor col-lg-6 col-md-12 text-center">
+                    <img src="./img/rodrigo-junco.jpg" alt="foto-perfil" className="img-fluid rounded-circle" />
+                </section>
 
-            <div className="acercaDe-contenedor">
-                <h2>Acerca de <span>Mi</span></h2>
-                <h3>Desarrollador Web Fullstack Jr</h3>
-                <p> 
-                    Hola! Me llamo Rodrigo Junco y me gustaria presentarme. 
-                    Soy un estudiante de Ingenieria en Sistemas de la Información
-                    en la Facultad Regional de Córdoba, UTN.
-                    Me desempeño como un Desarrollador Web Fullstack Jr.
-                    Mi objetivo como Desarrollador es implementar el 
-                    manejo correcto del software para desarrollar 
-                    aplicaciones que sean atractivas para los clientes.
-                </p>
-                <Btn contenido='Ver Mas' width='50%'/>
+                <div className="acercaDe-contenedor col-lg-6 col-md-12">
+                    <h2>Acerca de <span>Mi</span></h2>
+                    <h3>Desarrollador Web Fullstack Jr</h3>
+                    <p>
+                        Hola! Me llamo Rodrigo Junco y me gustaria presentarme.
+                        Soy un estudiante de Ingenieria en Sistemas de la Información
+                        en la Facultad Regional de Córdoba, UTN.
+                        Me desempeño como un Desarrollador Web Fullstack Jr.
+                        Mi objetivo como Desarrollador es implementar el
+                        manejo correcto del software para desarrollar
+                        aplicaciones que sean atractivas para los clientes.
+                    </p>
+                    <Btn contenido='Ver Mas' width='100%'/>
+                </div>
             </div>
-
         </section>
     )
 };
