345a8a867d70cd39fde2305ca199ec5278e2ce32
RPJunco <rodrigojunco023@gmail.com>
2024-07-19 22:58:36 -0300

    Se modifico el componente 'Header' utilizando bootstrap

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -3,15 +3,14 @@ import './Header.css';
 const Header = () => {
     return (    
         <header className="header fixed-top" id='header'>
-
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#"><img src="./img/logo.PNG" alt="logo-portofolio" /></a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <form className="d-flex" role="links">
-                        <ul className='nav'>
+                    <div className="collapse navbar-collapse" id="navbarScroll">
+                        <ul className="navbar-nav me-auto my-2 my-lg-0 d-flex w-100 justify-content-end navbar-nav-scroll" style={{ '--bs-scroll-height': '100px' }}>
                             <li className='nav-item'>
                                 <a href="#inicio" className='nav-link'>Inicio</a>
                             </li>
@@ -29,14 +28,12 @@ const Header = () => {
                             </li>
                             <li className='nav-item'>
                                 <a href="#contacto" className='nav-link'>Contacto</a>
-                                <i className="bi bi-list menu-icono"></i>
                             </li>
                         </ul>
-                    </form>
+                    </div>
                 </div>
             </nav>
-
-    </header>
+        </header>
     )
 };
 
