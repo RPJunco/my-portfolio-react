2b8de7e546703d45a59881f697b301b30424bbda
RPJunco <rodrigojunco023@gmail.com>
2024-07-20 14:54:39 -0300

    Se modifico la estructura del componente 'Work' utilizando bootstrap

--- src/Component/Main/Secciones/Work/Work.jsx
+++ src/Component/Main/Secciones/Work/Work.jsx
@@ -4,26 +4,35 @@ import Btn from '../../../Btn/Btn.jsx';
 const Work = () => {
     return (
         <section id="servicios" className='servicios'>
-
             <h2>Mis <span>Servicios</span></h2>
-
-            <section className="container-fluid">
-
-                <div className="card mb-4">
-                    <h3>Desarrollo de aplicaciones FrontEnd</h3>
-                    <Btn contenido='Leer Mas' width='50%'/>
+            <section className="container">
+                <div className="row">
+                    <div className="col-lg-4 col-md-6 mb-5">
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h3>Desarrollo de aplicaciones FrontEnd</h3>
+                                <Btn contenido='Leer Mas' width='70%'/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-6 mb-5">
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h3>Desarrollo de aplicaciones BackEnd</h3>
+                                <Btn contenido='Leer Mas' width='70%'/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-md-6 mb-5">
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h3>Desarrollo de aplicaciones FullStack</h3>
+                                <Btn contenido='Leer Mas' width='70%'/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="card mb-4">
-                    <h3>Desarrollo de aplicaciones BackEnd</h3>
-                    <Btn contenido='Leer Mas' width='50%'/>
-                </div>
-                <div className="card mb-4">
-                    <h3>Desarrollo de aplicaciones FullStack</h3>
-                    <Btn contenido='Leer Mas' width='50%'/>
-                </div>
-
             </section>
-
         </section>
     )
 };
