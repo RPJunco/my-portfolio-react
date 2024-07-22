1ec2ebf42452bd4e4d69329f22c440bb9fa12fe2
RPJunco <rodrigojunco023@gmail.com>
2024-07-21 22:38:03 -0300

    Se agrego un 'props' que permite especificar hacia donde se justifica el contenido del 'div' que contiene al 'button'

--- src/Component/Btn/Btn.jsx
+++ src/Component/Btn/Btn.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import './Btn.css'
 
-const Btn = ({ contenido, width }) => {
+const Btn = ({ contenido, width, position }) => {
     return (
-        <div className="d-grid" style={{ width:width }}>
+        <div className="d-grid" style={{ width:width , justifyContent:position }}>
             <button className="boton" type="button">{contenido}</button>
         </div>
     )
@@ -12,10 +12,12 @@ const Btn = ({ contenido, width }) => {
 Btn.propTypes = {
     contenido: PropTypes.string.isRequired,
     width: PropTypes.string.isRequired,
+    position: PropTypes.string.isRequired,
 };
 
 Btn.defaultProps = {
-    width: '100%'
+    width: '100%',
+    justifyContent: 'center'
 }
 
 
