a65c0f21636dce0db79385243824d873a4213415
RPJunco <rodrigojunco023@gmail.com>
2024-07-22 18:34:59 -0300

    Se modifico la estructura del componente 'Header' utilizando react-bootstrap

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,40 +1,38 @@
 import './Header.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Header = () => {
+function Header() {
     return (    
-        <header className="header fixed-top" id='header'>
-            <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img src="./img/logo.PNG" alt="logo-portofolio" /></a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarScroll">
-                        <ul className="navbar-nav me-auto my-2 my-lg-0 d-flex w-100 justify-content-end navbar-nav-scroll" style={{ '--bs-scroll-height': '100px' }}>
-                            <li className='nav-item'>
-                                <a href="#inicio" className='nav-link'>Inicio</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a href="#acercaDe" className='nav-link'>Acerca de</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a href="#servicios" className='nav-link'>Servicio</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a href="#skills" className='nav-link'>Habilidades</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a href="#portfolio" className='nav-link'>Portfolio</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a href="#contacto" className='nav-link'>Contacto</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    )
-};
+
+        <Navbar expand="xxl" className="fixed-top p-0" style={{ backgroundColor: '#070e1f'}}>
+            <Container fluid style={{ justifyContent: 'space-around' }}>
+                <Navbar.Brand href="#logo" style={{ padding: '0 4%' }}><img src="./img/logo.PNG" alt="logo-portofolio" /></Navbar.Brand> 
+                <Navbar.Toggle aria-controls="navbarScroll" style={{ backgroundColor: 'white' }}/>
+                <Navbar.Collapse id="navbarScroll" className='justify-content-end'>
+                <Nav
+                    className="my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <Nav.Link
+                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#inicio">Inicio</Nav.Link>
+                    <Nav.Link
+                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#acercaDe">Acerca de</Nav.Link>
+                    <Nav.Link
+                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#servicios">Servicios</Nav.Link>
+                    <Nav.Link
+                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#skills">Habilidades</Nav.Link>
+                    <Nav.Link
+                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#portfolio">Portfolio</Nav.Link>
+                    <Nav.Link
+                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#contacto">Contacto</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
 
 export default Header;
