122f46a5aece20597a056b9d0d8560b9041ae0f2
RPJunco <rodrigojunco023@gmail.com>
2024-07-22 09:00:18 -0300

    Se importaron direcciones que contiene paquetes de bootstrap al componente 'App'

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import Header from './Component/Header/Header.jsx'
 import Footer from './Component/Footer/Footer.jsx'
 import Main from './Component/Main/Main.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   
