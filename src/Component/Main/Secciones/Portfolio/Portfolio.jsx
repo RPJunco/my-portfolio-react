4e21a99e9dcd7fc647e18db7ccfcb69edfa981e5
RPJunco <rodrigojunco023@gmail.com>
2024-07-23 19:12:20 -0300

    Se modifico la estructura del componente 'Portfolio' utilizando react-bootstrap

--- src/Component/Main/Secciones/Portfolio/Portfolio.jsx
+++ src/Component/Main/Secciones/Portfolio/Portfolio.jsx
@@ -1,47 +1,51 @@
 import './Portfolio.css'
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Portfolio = () => {
     return (
-        <section id="portfolio" className="portfolio container-fluid">
-
-            <div className="text-center mb-5">
-                <h2 className="m-5">Mi <span>Portfolio</span></h2>
-            </div>
-
-            <div className="row">
-                <div className="col-lg-4 col-md-6 mb-4">
+        <Container 
+        fluid
+        style={{ padding: '100px 0px' }}
+        id='portfolio'
+        >
+            <Container className='text-center mb-5'>
+                <h2 className='m-5'>Mi <span>Portfolio</span></h2>
+            </Container>
+            
+            <Row className='mb-3'>
+                <Col lg={4} md={6} sm={12} className='mb-4'>
                     <a href="https://rpjunco.github.io/Travel-Agency-Login-page/" target="_blank">
-                        <img src="./img/portfolio/travel-agency-login-page.PNG" className="img-proyecto" alt="Travel-Agency-Login-page" />
+                        <img src="./img/portfolio/travel-agency-login-page.PNG" className="img-proyecto img-fluid" alt="Travel-Agency-Login-page" />
                     </a>
-                </div>
-                <div className="col-lg-4 col-md-6 mb-4">
+                </Col>
+                <Col lg={4} md={6} sm={12} className='mb-4'>
                     <a href="https://rpjunco.github.io/What-we-offer-section-design/" target="_blank">
-                        <img src="./img/portfolio/what-do-we-offer.PNG" className="img-proyecto" alt="What-dowe-offer" />
+                        <img src="./img/portfolio/what-do-we-offer.PNG" className="img-proyecto img-fluid" alt="What-dowe-offer" />
                     </a>
-                </div>
-                <div className="col-lg-4 col-md-6 mb-4">
+                </Col>
+                <Col lg={4} md={6} sm={12} className='mb-4'>
                     <a href="https://github.com/RPJunco/soccer-card-menu/tree/main" target="_blank">
-                        <img src="./img/portfolio/soccer-cards-menu.PNG" className="img-proyecto" alt="soccer-cards-menu" />
+                        <img src="./img/portfolio/soccer-cards-menu.PNG" className="img-proyecto img-fluid" alt="soccer-cards-menu" />
                     </a>
-                </div>
-                <div className="col-lg-4 col-md-6 mb-4">
+                </Col>
+                <Col lg={4} md={6} sm={12} className='mb-4'>
                     <a href="https://github.com/RPJunco/records-management/tree/main" target="_blank">
-                        <img src="./img/portfolio/records-management.PNG" className="img-proyecto" alt="records-management" />
+                        <img src="./img/portfolio/records-management.PNG" className="img-proyecto img-fluid" alt="records-management" />
                     </a>
-                </div>
-                <div className="col-lg-4 col-md-6 mb-4">
+                </Col>
+                <Col lg={4} md={6} sm={12} className='mb-4'>
                     <a href="https://github.com/RPJunco/character-and-list-processing/tree/main" target="_blank">
-                        <img src="./img/portfolio/character-treatment.PNG" className="img-proyecto" alt="character-and-list-processing" />
+                        <img src="./img/portfolio/character-treatment.PNG" className="img-proyecto img-fluid" alt="character-and-list-processing" />
                     </a>
-                </div>
-                <div className="col-lg-4 col-md-6 mb-4">
+                </Col>
+                <Col lg={4} md={6} sm={12} className='mb-4'>
                     <a href="https://github.com/RPJunco/article-management" target="_blank">
-                        <img src="./img/portfolio/strings-treatment.PNG" className="img-proyecto" alt="strings-treatment" />
+                        <img src="./img/portfolio/strings-treatment.PNG" className="img-proyecto img-fluid" alt="strings-treatment" />
                     </a>
-                </div>
-            </div>
+                </Col>
+            </Row>
 
-        </section>
+        </Container>
     )
 };
 
