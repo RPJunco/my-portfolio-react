cfe18cc4725246deb414b020315873cc89f3b184
RPJunco <rodrigojunco023@gmail.com>
2024-07-23 11:13:30 -0300

    Se modifico el componente 'Work' con react-bootstrap. Se elimino el archivo 'Work.css'

--- src/Component/Main/Secciones/Work/Work.jsx
+++ src/Component/Main/Secciones/Work/Work.jsx
@@ -1,39 +1,44 @@
-import './Work.css'
 import Btn from '../../../Btn/Btn.jsx';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Work = () => {
     return (
-        <section id="servicios" className='servicios'>
-            <h2>Mis <span>Servicios</span></h2>
-            <section className="container">
-                <div className="row">
-                    <div className="col-lg-4 col-md-6 mb-5">
-                        <div className="card h-100">
-                            <div className="card-body">
-                                <h3>Desarrollo de aplicaciones FrontEnd</h3>
-                                <Btn contenido='Leer Mas' width='70%'/>
+        <Container
+        fluid
+        id='servicios'
+        style={{ padding: '170px 20px' }}
+        className='servicios align-center mb-3'
+        >
+            <h2 style={{ margin: '0 0 130px 0' }}>Mis <span>Servicios</span></h2>
+            <Container>
+                <Row className='mb-5'>
+                    <Col lg={4} md={12} sm={12} className='mb-5 p-3'>
+                        <div className="card h-100 border-0">
+                            <div className="card-body" style={{ backgroundColor: '#293b50', borderRadius: '40px' }}>
+                                <h3 style={{ margin: '35px 0px', padding: '17px 0px' }}>Desarrollo de aplicaciones Frontend</h3>
+                                <Btn contenido='Leer Mas' width='60%' position='center'/>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 mb-5">
-                        <div className="card h-100">
-                            <div className="card-body">
-                                <h3>Desarrollo de aplicaciones BackEnd</h3>
-                                <Btn contenido='Leer Mas' width='70%'/>
+                    </Col>
+                    <Col lg={4} md={12} sm={12} className='mb-5 p-3'>
+                        <div className="card h-100 border-0">
+                            <div className="card-body" style={{ backgroundColor: '#293b50', borderRadius: '40px' }}>
+                                <h3 style={{ margin: '35px 0px', padding: '17px 0px' }}>Desarrollo de aplicaciones Backend</h3>
+                                <Btn contenido='Leer Mas' width='60%' position='center'/>
                             </div>
                         </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6 mb-5">
-                        <div className="card h-100">
-                            <div className="card-body">
-                                <h3>Desarrollo de aplicaciones FullStack</h3>
-                                <Btn contenido='Leer Mas' width='70%'/>
+                    </Col>
+                    <Col lg={4} md={12} sm={12} className='mb-5 p-3'>
+                        <div className="card h-100 border-0">
+                            <div className="card-body" style={{ backgroundColor: '#293b50', borderRadius: '40px' }}>
+                                <h3 style={{ margin: '35px 0px', padding: '17px 0px' }}>Desarrollo de aplicaciones Fullstack</h3>
+                                <Btn contenido='Leer Mas' width='60%' position='center'/>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </section>
-        </section>
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
     )
 };
 
