1989feda3627fe02c354b0a77fb3a0baf40d51ae
RPJunco <rodrigojunco023@gmail.com>
2024-07-23 19:37:49 -0300

    Se modifico la estructura del componente 'Work'

--- src/Component/Main/Secciones/Work/Work.jsx
+++ src/Component/Main/Secciones/Work/Work.jsx
@@ -1,5 +1,5 @@
 import Btn from '../../../Btn/Btn.jsx';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card, CardBody } from 'react-bootstrap';
 
 const Work = () => {
     return (
@@ -11,30 +11,30 @@ const Work = () => {
         >
             <h2 style={{ margin: '0 0 130px 0' }}>Mis <span>Servicios</span></h2>
             <Container>
-                <Row className='mb-5'>
-                    <Col lg={4} md={12} sm={12} className='mb-5 p-3'>
-                        <div className="card h-100 border-0">
-                            <div className="card-body" style={{ backgroundColor: '#293b50', borderRadius: '40px' }}>
+                <Row className='justify-content-center mb-5'>
+                    <Col lg={4} md={12} sm={12} className='mb-5 p-3 d-flex justify-content-center'>
+                        <Card className='h-100 w-auto' border='none' style={{ borderRadius: '40px', backgroundColor: '#293b50' }}>
+                            <CardBody backgroundColor='#293b50' borderRadius='40px'>
                                 <h3 style={{ margin: '35px 0px', padding: '17px 0px' }}>Desarrollo de aplicaciones Frontend</h3>
                                 <Btn contenido='Leer Mas' width='50%' position='center'/>
-                            </div>
-                        </div>
+                            </CardBody>
+                        </Card>
                     </Col>
-                    <Col lg={4} md={12} sm={12} className='mb-5 p-3'>
-                        <div className="card h-100 border-0">
-                            <div className="card-body" style={{ backgroundColor: '#293b50', borderRadius: '40px' }}>
+                    <Col lg={4} md={12} sm={12} className='mb-5 p-3 d-flex justify-content-center'>
+                        <Card className='h-100 w-auto' border='none' style={{ borderRadius: '40px', backgroundColor: '#293b50' }}>
+                            <CardBody backgroundColor='#293b50' borderRadius='40px'>
                                 <h3 style={{ margin: '35px 0px', padding: '17px 0px' }}>Desarrollo de aplicaciones Backend</h3>
                                 <Btn contenido='Leer Mas' width='50%' position='center'/>
-                            </div>
-                        </div>
+                            </CardBody>
+                        </Card>
                     </Col>
-                    <Col lg={4} md={12} sm={12} className='mb-5 p-3'>
-                        <div className="card h-100 border-0">
-                            <div className="card-body" style={{ backgroundColor: '#293b50', borderRadius: '40px' }}>
+                    <Col lg={4} md={12} sm={12} className='mb-5 p-3 d-flex justify-content-center'>
+                    <Card className='h-100 w-auto' border='none' style={{ borderRadius: '40px', backgroundColor: '#293b50' }}>
+                            <CardBody backgroundColor='#293b50' borderRadius='40px'>
                                 <h3 style={{ margin: '35px 0px', padding: '17px 0px' }}>Desarrollo de aplicaciones Fullstack</h3>
                                 <Btn contenido='Leer Mas' width='50%' position='center'/>
-                            </div>
-                        </div>
+                            </CardBody>
+                        </Card>
                     </Col>
                 </Row>
             </Container>
