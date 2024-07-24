be61935bc9aff8d09d86e97aba0f57df20cab72c
RPJunco <rodrigojunco023@gmail.com>
2024-07-23 21:32:15 -0300

    Se añadio una funcionalidad para manejar la posicion del boton en el componente 'Hero'

--- src/Component/Main/Secciones/Hero/Hero.jsx
+++ src/Component/Main/Secciones/Hero/Hero.jsx
@@ -1,7 +1,31 @@
 import Btn from '../../../Btn/Btn.jsx';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Image, Card } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
 
 const Hero = () => {
+
+    const[btnPosition, setBtnPosition] = useState('center');
+
+    useEffect(() => {
+        const manejarTamanio = () => {
+            const ancho = window.innerWidth;
+            console.log(ancho);
+            if (ancho >= 1400){
+                setBtnPosition('start');
+            }else{
+                setBtnPosition('center');
+            }
+        };
+
+        manejarTamanio();
+
+        window.addEventListener('resize', manejarTamanio);
+
+        return () => {
+            window.removeEventListener('resize', manejarTamanio);
+        };
+    }, []);
+
     return (
         <Container 
         fluid
@@ -10,30 +34,37 @@ const Hero = () => {
         style={{ padding:'170px 0px' }}
         >
             <Row className='justify-content-center align-items-center mb-5'>
-                <Col lg={6} md={12}>
-                    <div className="hero-section-info"> 
-                        <div className="text-start">
-                            <h3>Hola de nuevo, soy yo!</h3>
-                            <h1>Rodrigo Junco</h1>
-                            <h2>Y soy un <span>Desarrollador Web Fullstack Jr</span></h2>
-                            <p>Me encargo de desarrollar aplicaciones web que coinciden con los requerimientos de los clientes.</p>
-                        </div>
-                        <div className="hero-iconos-contenedor text-start">
-                            <a href="https://www.instagram.com/rodrigojunco.23/" target="_blank">
+                <Col lg={6} md={12} className='mb-5'>
+                    <Card 
+                    className='p-5 text-xxl-start text-center' border='0' style={{ backgroundColor: '#011225' }}
+                    >
+                        <h3>Hola de nuevo, soy yo!</h3>
+                        <h1>Rodrigo Junco</h1>
+                        <h2>Y soy un <span>Desarrollador Web Fullstack Jr</span></h2>
+                        <Card.Text className='text-xxl-start text-center'>
+                            Me encargo de desarrollar aplicaciones web que coinciden con los requerimientos de los clientes.
+                        </Card.Text>
+                        <Card 
+                        className='hero-iconos-contenedor d-flex justify-content-xxl-start align-items-xxl-start justify-content-center align-items-center flex-row' 
+                        style={{ backgroundColor: '#011225' }}
+                        >
+                            <Card.Link href="https://www.instagram.com/rodrigojunco.23/" target="_blank">
                                 <i className="bi bi-instagram"></i>
-                            </a>
-                            <a href="https://www.linkedin.com/in/rodrigojuncodev/" target="_blank">
+                            </Card.Link>
+                            <Card.Link href="https://www.linkedin.com/in/rodrigojuncodev/" target="_blank">
                                 <i className="bi bi-linkedin"></i>
-                            </a>
-                            <a href="https://github.com/RPJunco" target="_blank">
+                            </Card.Link>
+                            <Card.Link href="https://github.com/RPJunco" target="_blank">
                                 <i className="bi bi-github"></i>
-                            </a>
-                        </div>
-                        <Btn contenido="Descargar CV" width="50%" position='start'/>
-                    </div>
+                            </Card.Link>
+                        </Card>
+                        <Card style={{ backgroundColor: '#011225' }}>
+                            <Btn contenido="Descargar CV" width="50%" position={btnPosition}/>
+                        </Card>
+                    </Card>
                 </Col>
-                <Col lg={6} md={12} className='text-center'>
-                    <img src="./img/rodrigo-junco.jpg" alt="foto-perfil" className="foto-contenedor rounded-circle" />
+                <Col lg={6} md={12} className='text-center d-xxl-block d-none'>
+                    <Image src="./img/rodrigo-junco.jpg" alt="foto-perfil" style={{ boxShadow: '0 0 40px rgb(134, 255, 255, 0.9' }} roundedCircle fluid width='70%'/>
                 </Col>
             </Row>
         </Container>
