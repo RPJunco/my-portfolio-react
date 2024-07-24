cf55ab14ede744ba58ab69ac81dd95c0eecf2e7c
RPJunco <rodrigojunco023@gmail.com>
2024-07-23 21:30:44 -0300

    Se realizaron cambios en el componente 'Btn', con el objetivo de proporcionar un posicionamiento mas especifico. Esto se reflejara de mejor manera con los componentes 'Hero' y 'About'

--- src/Component/Btn/Btn.jsx
+++ src/Component/Btn/Btn.jsx
@@ -2,8 +2,11 @@ import PropTypes from 'prop-types';
 import './Btn.css'
 
 const Btn = ({ contenido, width, position }) => {
+
+    const alignment = position === 'start' ? 'justify-content-start' : 'justify-content-center';
+
     return (
-        <div className="d-grid w-100" style={{ width:width , justifyContent:position }}>
+        <div className={`d-grid w-100 ${alignment}`} style={{ width:width }}>
             <button className="boton" type="button">{contenido}</button>
         </div>
     )
