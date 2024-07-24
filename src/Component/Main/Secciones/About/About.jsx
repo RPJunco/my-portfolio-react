346dc93b516d163ac09f1a5bd6e6920828bbb6ef
RPJunco <rodrigojunco023@gmail.com>
2024-07-23 22:41:53 -0300

    Se añadio la funcionalidad de poder posicionar el componente 'Btn' de acuerdo al tamaño de la pagina, ademas de eso se elimino una propiedad en la linea 39

--- src/Component/Main/Secciones/About/About.jsx
+++ src/Component/Main/Secciones/About/About.jsx
@@ -1,23 +1,47 @@
 import Btn from '../../../Btn/Btn.jsx'
 import { Container, Row, Col, Image, Card } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
 
 const About = () => {
+
+    const[btnPosition, setBtnPosition] = useState('center');
+
+    useEffect(() => {
+        const manejarTamanio = () => {
+            const ancho = window.innerWidth;
+            if (ancho >= 1400){
+                setBtnPosition('start');
+            }else{
+                setBtnPosition('center');
+            }
+        };
+
+        manejarTamanio();
+
+        window.addEventListener('resize', manejarTamanio);
+
+        return () => {
+            window.removeEventListener('resize', manejarTamanio);
+        };
+    }, []);
+
     return (
         <Container 
         fluid 
         id='acercaDe'
         style={{ backgroundColor: '#293b50', padding: '170px 0px' }}
         >
-            <Row className='align-items-center justify-content-center'>
-                <Col lg={6} md={12} className='text-center foto-contenedor'>
+            <Row className='justify-content-center align-items-center mb-5'>
+                <Col lg={6} md={12} className='text-center d-xxl-block d-none'>
                     <Image src="./img/rodrigo-junco.jpg" alt="foto-perfil" style={{ boxShadow: '0 0 40px rgb(134, 255, 255, 0.9' }} roundedCircle fluid width='70%'/>
                 </Col>
-                <Col lg={6} md={12} style={{ width: '40%', padding: '50px 0px' }}>
-                    <Card 
+                <Col lg={6} md={12}
+                style={{ padding: '50px 0px' }}>
+                    <Card className='justify-content-center text-center text-xxl-start'
                     style={{ backgroundColor: '#293b50' }} border='0'>
-                        <Card.Body className='justify-content-center'>
-                            <h2>Acerca de <span>Mi</span></h2>
-                            <h3>Desarrollador Web Fullstack Jr</h3>
+                        <Card.Body className='justify-content-center text-center text-xxl-start'>
+                            <h2 className='pb-0 mb-0'>Acerca de <span>Mi</span></h2>
+                            <h3 className='pb-3'>Desarrollador Web Fullstack Jr</h3>
                             <Card.Text className='justify-content-center' color='white' style={{ textAlign: 'justify' }}>
                                 Hola! Me llamo Rodrigo Junco y me gustaria presentarme.
                                 Soy un estudiante de Ingenieria en Sistemas de la Información
@@ -27,7 +51,7 @@ const About = () => {
                                 manejo correcto del software para desarrollar
                                 aplicaciones que sean atractivas para los clientes.
                             </Card.Text>
-                            <Btn contenido='Ver Mas' width='50%' position='center'/>
+                            <Btn contenido='Ver Mas' width='50%' position={btnPosition}/>
                         </Card.Body>
                     </Card>
                 </Col>
