a1ca1e74baf32451c8192159d0ccb41110ffda02
RPJunco <rodrigojunco023@gmail.com>
2024-07-24 23:13:36 -0300

    Se cambio la forma de importar los componentes de react-bootstrap

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,9 +1,6 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-scroll'
 import { useState, useEffect } from 'react';
-import Image from 'react-bootstrap/Image';
+import { Image, Container, Nav, Navbar } from 'react-bootstrap';
 
 function Header() {
 
