d280597c992157a058da6faab49d0a107e8ec3c2
RPJunco <rodrigojunco023@gmail.com>
2024-07-24 22:07:48 -0300

    Se modifico la estructura del componente 'Header'. Se importo un componente Link para cada uno de los link del navbar, tambien se importaron Hooks (useState, useEffect) para aplicar funcionalidades al componente. La funcionalidad se basa en adaptar algunos aspectos (padding, offset) de acuerdo al ancho del dispositivo

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,10 +1,45 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-scroll'
+import { useState, useEffect } from 'react';
 
 function Header() {
-    return (    
 
+    const[offSetCustom, setOffSetCustom] = useState(0);
+    const[paddingCustom, setPaddingCustom] = useState('35px 35px')
+
+    useEffect(() => {
+        const manejarTamanio = () => {
+            const ancho = window.innerWidth;
+            if (ancho >= 1400){
+                setOffSetCustom(0);
+                setPaddingCustom('35px 35px');
+            }else if(ancho >= 940){
+                setOffSetCustom(-60);
+                setPaddingCustom('30px 400px');
+            }else if (ancho >= 550){
+                setOffSetCustom(-90);
+                setPaddingCustom('30px 200px');
+            } else if(ancho >= 380){
+                setOffSetCustom(-120);
+                setPaddingCustom('30px 125px');
+            }else{
+                setOffSetCustom(-140);
+                setPaddingCustom('30px 90px');
+            }
+        };
+
+        manejarTamanio();
+
+        window.addEventListener('resize', manejarTamanio);
+
+        return () => {
+            window.removeEventListener('resize', manejarTamanio);
+        };
+    }, []);
+
+    return (    
         <Navbar expand="xxl" className="fixed-top p-0" style={{ backgroundColor: '#070e1f'}}>
             <Container fluid style={{ justifyContent: 'space-around' }}>
                 <Navbar.Brand href="#logo" style={{ padding: '0 4%' }}><img src="./img/logo.PNG" alt="logo-portofolio" /></Navbar.Brand> 
@@ -15,18 +50,60 @@ function Header() {
                     style={{ maxHeight: '100px' }}
                     navbarScroll
                 >
-                    <Nav.Link
-                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#inicio">Inicio</Nav.Link>
-                    <Nav.Link
-                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#acercaDe">Acerca de</Nav.Link>
-                    <Nav.Link
-                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#servicios">Servicios</Nav.Link>
-                    <Nav.Link
-                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#skills">Habilidades</Nav.Link>
-                    <Nav.Link
-                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#portfolio">Portfolio</Nav.Link>
-                    <Nav.Link
-                    style={{ padding: '30px 35px', color: 'white', fontSize: '23px' }} href="#contacto">Contacto</Nav.Link>
+                    <Nav.Item
+                    style={{ padding: '30px 0px', color: 'white', fontSize: '23px' }}>
+                        <Link
+                        to="inicio" spy={true} smooth={true} offset={offSetCustom} duration={300} 
+                        style={{ padding: paddingCustom }}
+                        >
+                            Inicio
+                        </Link>
+                    </Nav.Item>
+                    <Nav.Item
+                    style={{ padding: '30px 0px', color: 'white', fontSize: '23px' }}>
+                        <Link 
+                        to='acercaDe' spy={true} smooth={true} offset={offSetCustom} duration={300}
+                        style={{ padding: paddingCustom }}
+                        >
+                            Acerca De
+                        </Link>
+                    </Nav.Item>
+                    <Nav.Item
+                    style={{ padding: '30px 0px', color: 'white', fontSize: '23px' }}>
+                        <Link 
+                        to='servicios' spy={true} smooth={true} offset={offSetCustom} duration={300}
+                        style={{ padding: paddingCustom }}
+                        >
+                            Servicios
+                        </Link>
+                    </Nav.Item>
+                    <Nav.Item
+                    style={{ padding: '30px 0px', color: 'white', fontSize: '23px' }}>
+                        <Link 
+                        to='skills' spy={true} smooth={true} offset={offSetCustom} duration={300}
+                        style={{ padding: paddingCustom }}
+                        >
+                            Habilidades
+                        </Link>
+                    </Nav.Item>
+                    <Nav.Item
+                    style={{ padding: '30px 0px', color: 'white', fontSize: '23px' }}>
+                        <Link 
+                        to='portfolio' spy={true} smooth={true} offset={offSetCustom} duration={300}
+                        style={{ padding: paddingCustom }}
+                        >
+                            Portfolio
+                        </Link>
+                    </Nav.Item>
+                    <Nav.Item
+                    style={{ padding: '30px 0px', color: 'white', fontSize: '23px' }}>
+                        <Link 
+                        to='contacto' spy={true} smooth={true} offset={offSetCustom} duration={300}
+                        style={{ padding: paddingCustom }}
+                        >
+                            Contacto
+                        </Link>
+                    </Nav.Item>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
