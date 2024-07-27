2ad760dda6e8ead64b86bd209792d24697b30fc5
RPJunco <rodrigojunco023@gmail.com>
2024-07-27 20:27:07 -0300

    Se añadio un prop al componente 'Btn'. El mismo permite definir que tipo de boton es

--- src/Component/Btn/Btn.jsx
+++ src/Component/Btn/Btn.jsx
@@ -1,24 +1,26 @@
 import PropTypes from 'prop-types';
 import './Btn.css'
 
-const Btn = ({ contenido, width, position }) => {
+const Btn = ({ tipo, contenido, width, position }) => {
 
     const alignment = position === 'start' ? 'justify-content-start' : 'justify-content-center';
 
     return (
         <div className={`d-grid w-100 ${alignment}`} style={{ width:width }}>
-            <button className="boton" type="button">{contenido}</button>
+            <button className="boton" type={tipo}>{contenido}</button>
         </div>
     )
 };
 
 Btn.propTypes = {
+    tipo: PropTypes.string.isRequired,
     contenido: PropTypes.string.isRequired,
     width: PropTypes.string.isRequired,
     position: PropTypes.string.isRequired,
 };
 
 Btn.defaultProps = {
+    tipo: 'button',
     width: '100%',
     justifyContent: 'center'
 }
