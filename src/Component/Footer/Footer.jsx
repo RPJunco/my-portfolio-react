60cfd88d742191e9eaa2ca7da6a61164c9fe6e60
RPJunco <rodrigojunco023@gmail.com>
2024-07-27 19:56:13 -0300

    Se elimino el archivo 'Footer.css'. Se modifico la estructura del componente 'Footer' utilizando Formik y Yup para el formulario y su validacion.

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,41 +1,144 @@
-import './Footer.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Btn from '../Btn/Btn.jsx'
+import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Container, Row, Col } from 'react-bootstrap'
+
 
 const Footer = () => {
+
+    const validationSchema = Yup.object({
+        nombre: Yup.string()
+        .required('Nombre es requerido')
+        .matches(/^[a-zA-Z\s]+$/, 'El nombre solo puede contener letras y espacios')
+        .min(3, 'Debe ingresar un minimo de 3 caracteres'),
+
+        apellido: Yup.string()
+        .required('Apellido es requerido')
+        .matches(/^[a-zA-Z\s]+$/, 'El apellido solo puede contener letras y espacios')
+        .min(3, 'Debe ingresar un minimo de 3 caracteres'),
+        
+        telefono: Yup.string()
+        .required('Telefono es requerido')
+        .matches(/^\d+$/, 'No se pueden ingresar letras, solo numeros')
+        .min(10, 'El teléfono debe tener al menos 10 dígitos')
+        .max(15, 'El teléfono no puede tener más de 15 dígitos'),
+        
+        email: Yup.string()
+        .email('Ingrese un Email valido')
+        .required('Email es requerido'),
+        
+        mensaje: Yup.string()
+        .required('Comentario es requerido')
+        .min(10, 'El comentario no puede tener menos de 10 caracteres')
+        .max(400, 'El comentario no puede tener mas de 350 caracteres')
+    });
+
     return (
-        <footer className='footer' id='contacto'>
-            
-            <div className='container-fluid justify-content-center align-items-center w-75'>
+        <footer 
+        className='container-fluid footer' 
+        id='contacto'
+        style={{backgroundColor: '#293b50', padding: '130px 0px'}}
+        >
+
+            <Container 
+            className='justify-content-center align-items-center'
+            >
                 <h2>Interesante verdad? <span>Contactame!</span></h2>
-
-                <div className='container-fluid justify-content-center align-items-center flex-wrap w-75 p-3'>
-
-                    <div className="form-floating">
-                        <input type="text" className="form-control" id="floatingName" placeholder="Pedro" />
-                        <label htmlFor="floatingName">Nombre</label>
-                    </div>
-                    <div className="form-floating">
-                        <input type="text" className="form-control" id="floatingLastName" placeholder="Pascal" />
-                        <label htmlFor="floatingLastName">Apellido</label>
-                    </div>
-                    <div className="form-floating">
-                        <input type="tel" className="form-control" id="floatingPhoneNumber" placeholder="351*******" />
-                        <label htmlFor="floatingPhoneNumber">Telefono de contacto</label>
-                    </div>
-                    <div className="form-floating">
-                        <input type="email" className="form-control" id="floatingEmail" placeholder="Email" />
-                        <label htmlFor="floatingEmail">Direccion de email</label>
-                    </div>
-
-                </div>
-
-                <div className="form-floating form-container container-fluid justify-content-center w-75">
-                    <textarea className="form-control" placeholder="Deja un comentario!!" id="floatingTextarea"></textarea>
-                    <label htmlFor="floatingTextarea" className='label-textarea'>Comments</label>
-                </div>
-                <Btn contenido='Enviar Mensaje' width='25%'/>
-            </div>
+                <Formik
+                initialValues={{ nombre: '', apellido: '', telefono: '', email: '', mensaje: '' }}
+                validationSchema={validationSchema}
+                onSubmit={(values, { resetForm }) => {
+                    console.log('Formulario enviado', values);
+                    resetForm(); 
+                }}
+                >
+                    {({
+                        isSubmitting, handleSubmit, touched
+                    }) => (
+                        <Container>
+                            <Form className='d-flex flex-column justify-content-center align-items-center' onSubmit={handleSubmit} noValidate>
+                                <Row className='w-75 justify-content-center mb-2'>
+                                    <Col lg={6} md={6} className='mb-2'>
+                                        <div className="form-floating">
+                                            <Field 
+                                            type="text" 
+                                            name="nombre" 
+                                            className={`form-control ${touched.nombre && 'is-invalid'}`} 
+                                            id="nombre" 
+                                            autoComplete='given-name'
+                                            aria-describedby='nombreError'
+                                            />
+                                            <label htmlFor="nombre">Nombre</label>
+                                            <ErrorMessage name="nombre" component="div" className="text-danger" id='nombreError'/>
+                                        </div>
+                                    </Col>
+                                    <Col lg={6} md={6} className='mb-2'>
+                                        <div className="form-floating">
+                                            <Field 
+                                            type="text" 
+                                            name='apellido' 
+                                            className={`form-control ${touched.apellido && 'is-invalid'}`} 
+                                            id="apellido" 
+                                            autoComplete='family-name'
+                                            aria-describedby="apellidoError"
+                                            />
+                                            <label htmlFor="apellido">Apellido</label>
+                                            <ErrorMessage name="apellido" component="div" className="text-danger" id='apellidoError'/>                                        
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row className='w-75 justify-content-center mb-2'>
+                                    <Col lg={6} md={6} className='mb-2'>              
+                                        <div className="form-floating">
+                                            <Field 
+                                            type="tel" 
+                                            name='telefono' 
+                                            className={`form-control ${touched.telefono && 'is-invalid'}`} 
+                                            id="tel" 
+                                            autoComplete='tel'
+                                            aria-describedby="telError"
+                                            />
+                                            <label htmlFor="tel">Telefono de contacto</label>
+                                            <ErrorMessage name="telefono" component="div" className="text-danger" id='telError'/>                                    
+                                        </div>                                                                  
+                                    </Col>
+                                    <Col lg={6} md={6} className='mb-2'>         
+                                    <div className="form-floating">
+                                        <Field 
+                                        type="email" 
+                                        name='email' 
+                                        className={`form-control ${touched.email && 'is-invalid'}`} 
+                                        id="email" 
+                                        autoComplete='email'
+                                        aria-describedby="emailError"
+                                        />
+                                        <label htmlFor="email">Direccion de email</label>
+                                        <ErrorMessage name="email" component="div" className="text-danger" id='emailError'/>                   
+                                    </div>                              
+                                    </Col>
+                                </Row>
+                                <Row className='w-75 justify-content-center mb-4'>
+                                    <Col lg={12}>
+                                        <div className="form-floating form-container justify-content-center w-100">
+                                            <Field 
+                                            as="textarea" 
+                                            name="mensaje" 
+                                            className={`form-control w-100 ${touched.mensaje && 'is-invalid'}`} 
+                                            id="mensaje" 
+                                            autoComplete='off'
+                                            aria-describedby="mensajeError"
+                                            />
+                                            <label htmlFor="mensaje">Deje su mensaje</label>
+                                        </div>
+                                        <ErrorMessage name="mensaje" component="div" className="text-danger" id='mensajeError'/>  
+                                        <Btn tipo='submit' disabled={isSubmitting} contenido='Enviar Mensaje' width='25%' position='center'/>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </Container>
+                    )}
+                </Formik>
+            </Container> 
         </footer>
     )
 };
