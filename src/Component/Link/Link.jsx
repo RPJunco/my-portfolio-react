2928306f6da0be80be29bb678849852d3efb982e
RPJunco <rodrigojunco023@gmail.com>
2024-07-28 12:35:59 -0300

    Se creo un componente llamado 'Link'. Este componente cumple la funcion de un boton pero en su estructura contiene una etiqueta 'a', ademas utiliza lo estilos del componente 'Btn'

---
+++ src/Component/Link/Link.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import '../Btn/Btn.css';
+
+const Link = ({ contenido, width, position, link, download }) => {
+
+    const alignment = position === 'start' ? 'justify-content-start' : 'justify-content-center';
+
+    return (
+        <div className={`d-grid w-100 ${alignment}`} style={{ width:width }}>
+            <a href={link} download={download} target='_blank' className='boton'>{contenido}</a>
+        </div>
+    )
+};
+
+Link.propTypes = {
+    contenido: PropTypes.string.isRequired,
+    width: PropTypes.string.isRequired,
+    position: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    download: PropTypes.string.isRequired
+};
+
+Link.defaultProps = {
+    tipo: 'button',
+    width: '100%',
+    justifyContent: 'center',
+    link: '#',
+    download: false,
+}
+
+
+export default Link;
